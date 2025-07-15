71caa9d62470e1903a208b4f36a199d46a70b830
Honzapkcz <honzapkc@gmail.com>
2025-07-15 21:01:04 +0200

    tvoje mama

--- www/home.php
+++ www/home.php
@@ -1,54 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
 <?php
-	require "Medoo.php";
-	require "./phpgraphlib.php";
-	use Medoo\Medoo;
-	class data_set
-	{
-		public $column_name;
-		public $data;
-		public $times;
-		public $color;
-		function __construct($database, $column, $color) {
-			$this->column_name = $column;
-			$this->color = $color;
-			$this->data = [];
-			$this->times = [];
-		
-			$database_select = $database->select("data", array("time",$column), ["LIMIT" => [0, 144]]); #last 12h of data
-			foreach ($database_select as $column_data) {
-				array_push($this->data,$column_data[$column]);
-				array_push($this->times,$column_data["time"]);
-			}
-		}
+require "Medoo.php";
+require "./phpgraphlib.php";
+use Medoo\Medoo;
+# CREATE TABLE data (id INTEGER PRIMARY KEY AUTOINCREMENT, time INTEGER DEFAULT CURRENT_TIMESTAMP, temperature INTEGER, humidity INTEGER, wind INTEGER, water INTEGER);
+$db = new Medoo([
+	"type" => "sqlite",
+	"database" => "data.db",
+]);
+
+?>
+<title>Weather Station</title>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<main>
+<form action="home.php" method="get">
+	<div class="nav">
+		<input type="submit" name="action" value="Overview">
+		<input type="submit" name="action" value="Temperature">
+		<input type="submit" name="action" value="Humidity">
+		<input type="submit" name="action" value="Wind Speed">
+		<input type="submit" name="action" value="Water Level">
+		<input type="submit" name="action" value="Insert Value">
+	</div>
+</form>
+<div class="content">
+<?php
+$h = $_GET["action"];
+$columns = [];
+echo "<h1>$h</h1>";
+
+if ($h == "Insert Value") {
+	if ($_GET["temperature"]) {
+		echo "<p>Data Inserted</p>";
+		$db->insert("data", [
+			"temperature" => $_GET["temperature"],
+			"humidity" => $_GET["humidity"],
+			"water" => $_GET["water"],
+			"wind" => $_GET["wind"],
+		]);
 	}
-	function create_graph($file_name, $x_size, $y_size, $data) {
-		$graph = new PHPGraphLib($x_size, $y_size, $file_name);
+	include "insert.php";
+} else {
+	switch ($h) {
+	case "Overview":
+		$columns = ["time", "temperature", "humidity", "water", "wind"];
+		break;
+	case "Temperature":
+		$columns = ["time", "temperature"];
+		break;
+	case "Humidity":
+		$columns = ["time", "humidity"];
+		break;
+	case "Wind Speed":
+		$columns = ["time", "wind"];
+		break;
+	case "Water Level":
+		$columns = ["time", "water"];
+		break;
+	}
+	if ($columns != []) {
+		$data = $db->select("data", $columns);
+
+?>
+<img src="./image.png">
+<?php
+
+		echo "<table><tr>";
+
+		foreach ($columns as $col) {
+			echo "<th>$col</th>";
+		}
+		echo "</tr>";
+		$gd = [];
+		foreach ($data as $row) {
+			echo "<tr>";
+			foreach ($columns as $col) {
+				echo "<td>$row[$col]</td>";
+			}
+
+				array_push($gd, $row[$columns[1]]);
+			echo "</tr>";
+		}
+		echo "</table>";
+
+		$graph = new PHPGraphLib(500, 250,"image.png");
+
+		$orange = "#f50";
+
 
 		$graph->setTextColor("white");
 		$graph->setBackgroundColor("black");
+
+		$graph->setDataValues(true);
+		$graph->setDataValueColor($orange);
+
 		$graph->setBars(false);
 
+		$graph->setDataPoints(true);
+		$graph->setDataPointColor($orange);
+
 		$graph->setLine(true);
+		$graph->setLineColor($orange);
 
-		foreach ($data as $data_set) {
-			$graph->addData($data_set->data);
-			$graph->setLineColor($data_set->color);
-		}
+
+		$graph->addData($gd);
+		$graph->setTitle($h);
 		$graph->createGraph();
+
 	}
+}
 
-
-
-
-	$db = new Medoo([
-		"type" => "sqlite",
-		"database" => "data.db",
-	]);
-	$data = array(
-		new data_set($db, "temperature",	"#f00"),
-		new data_set($db, "humidity",		"#0fa")
-	);
-
-	create_graph("img.png",300,200,$data);
 ?>
-<img src="./img.png">
+</div>
+</main>
+
+</body>
+</html>
